eb24969bcf2ae2b0ccdac3ccc747ce2625ccb795
jzc233 <63482703+jzc233@users.noreply.github.com>
2024-12-08 13:50:01 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_dipper.mk
+    $(LOCAL_DIR)/twrp_equuleus.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_dipper-userdebug \
-    twrp_dipper-eng
+    twrp_equuleus-userdebug \
+    twrp_equuleus-eng
